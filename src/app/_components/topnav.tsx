7f9919bb866feaae92c95e72bae53220654af89d
Theo Browne <me@t3.gg>
2024-01-15 21:18:01 -0800

    working auth

--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -1,5 +1,9 @@
+import { UserButton } from "@clerk/nextjs";
+
 export const Topnav = () => {
   return (
-    <div className="min-h-0 bg-zinc-900 p-4 text-2xl shadow-xl">Gallery</div>
+    <div className="flex min-h-0 justify-between bg-zinc-900 p-4 text-2xl shadow-xl">
+      Gallery <UserButton afterSignOutUrl="/" />
+    </div>
   );
 };
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "~/styles/globals.css";
 
 import { Inter } from "next/font/google";
 import { Topnav } from "./_components/topnav";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -20,13 +21,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body
-        className={`font-sans ${inter.variable} grid h-screen grid-rows-[auto_1fr]`}
-      >
-        <Topnav />
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body
+          className={`font-sans ${inter.variable} grid h-screen grid-rows-[auto_1fr]`}
+        >
+          <Topnav />
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
