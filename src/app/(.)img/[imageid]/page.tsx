21163106ecd6d5fea88a98689960af1d684f293c
Theo Browne <me@t3.gg>
2024-02-23 23:53:42 -0800

    clerk layout shift

--- src/app/(.)img/[imageid]/page.tsx
+++ src/app/(.)img/[imageid]/page.tsx
@@ -1,9 +1,14 @@
-import { Dialog } from "~/components/ui/dialog";
+"use client";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "~/components/ui/dialog";
 
 export default function ImagePage(props: { params: { imageid: string } }) {
-  return (
-    <Dialog>
-      <div>ImagePage</div>
-    </Dialog>
-  );
+  return <div>This is an image</div>;
 }
--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -3,7 +3,7 @@ import { UserButton } from "@clerk/nextjs";
 export const Topnav = () => {
   return (
     <div className="flex min-h-0 justify-between bg-zinc-900 p-4 text-2xl shadow-xl">
-      Gallery <UserButton afterSignOutUrl="/" />
+      {/* Gallery <UserButton afterSignOutUrl="/" /> */}
     </div>
   );
 };
