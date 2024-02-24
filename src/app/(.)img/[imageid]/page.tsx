651db4fbab9f783857c0001b28a05f90f7c357ec
Theo Browne <me@t3.gg>
2024-02-23 21:21:44 -0800

    shadcn + img page template

---
+++ src/app/(.)img/[imageid]/page.tsx
@@ -0,0 +1,9 @@
+import { Dialog } from "~/components/ui/dialog";
+
+export default function ImagePage(props: { params: { imageid: string } }) {
+  return (
+    <Dialog>
+      <div>ImagePage</div>
+    </Dialog>
+  );
+}
