0318e3ad8809ac78df0a6530e9e6feb7419722cb
Theo Browne <me@t3.gg>
2024-03-09 00:40:43 -0800

    turbopack bug

--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -1,11 +1,18 @@
-import { UserButton } from "@clerk/nextjs";
+import { SignedIn, UserButton } from "@clerk/nextjs";
 import Link from "next/link";
+import { UploadButton } from "~/components/uploadthing";
 
 export const Topnav = () => {
   return (
     <div className="flex min-h-0 justify-between border-b p-4 text-2xl shadow-xl">
       <Link href="/">Gallery</Link>
-      <UserButton afterSignOutUrl="/" />
+
+      <div>
+        <SignedIn>
+          <UploadButton endpoint="imageUploader" />
+        </SignedIn>
+        <UserButton afterSignOutUrl="/" />
+      </div>
     </div>
   );
 };
